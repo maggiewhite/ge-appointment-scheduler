d77eacb027d95315630e4e9f51f6be39a9ac991a
Maggie White <maggie@dssd.com>
2016-01-26 04:22:10 -0800

    Up and functional including scheduling appointments. No email

--- ge-cancellation-checker.phantom.js
+++ ge-cancellation-checker.phantom.js
@@ -1,26 +1,22 @@
-
-// CLI usage:
-// phantomjs [--ssl-protocol=any] goes-checker.js [-v|--verbose]
-
 var system = require('system');
 var fs = require('fs');
 
 var VERBOSE = false;
 var loadInProgress = false;
-
+// XXX: log to file not console
+// XXX: send text/email notifications when rescheduling
+// XXX: accept arguments to automatically recheck and schedule vs not schedule
 // Calculate path of this file
-var PWD = '';
-var current_path_arr = system.args[0].split('/');
-if (current_path_arr.length == 1) { PWD = '.'; }
-else {
-    current_path_arr.pop();
-    PWD = current_path_arr.join('/');
-}
+var PWD = fs.workingDirectory;
 
+console.log("testing1");
 // Gather Settings...
 try {
     var settings = JSON.parse(fs.read(PWD + '/config.json'));
-    if (!settings.username || !settings.username || !settings.init_url || !settings.enrollment_location_id) {
+    if (!settings.logfile)
+        console.log('No logfile specified. Please specify logfile in config.json');
+    var logfile = PWD + '/' + settings.logfile;
+    if (!settings.username || !settings.password || !settings.init_url || !settings.enrollment_location_id) {
         console.log('Missing username, password, enrollment location ID, and/or initial URL. Exiting...');
         phantom.exit();
     }
@@ -30,21 +26,9 @@ catch(e) {
     phantom.exit();
 }
 
-// ...from command
-system.args.forEach(function(val, i) {
-    if (val == '-v' || val == '--verbose') { VERBOSE = true; }
-});
-
-function fireClick(el) {
-    var ev = document.createEvent("MouseEvents");
-    ev.initEvent("click", true, true);
-    el.dispatchEvent(ev);
-}
-
 var page = require('webpage').create();
-
+page.open(settings.init_url);
 page.onConsoleMessage = function(msg) {
-    if (!VERBOSE) { return; }
     console.log(msg);
 };
 
@@ -56,13 +40,13 @@ page.onError = function(msg, trace) {
 page.onCallback = function(query, msg) {
     if (query == 'username') { return settings.username; }
     if (query == 'password') { return settings.password; }
-    if (query == 'fireClick') {
-        return function() { return fireClick; } // @todo:david DON'T KNOW WHY THIS DOESN'T WORK! :( Just returns [Object object])
-    }
+    if (query == 'enrollment_location_id') { return settings.enrollment_location_id.toString(); }
+    if (query == 'cur_date') { if (msg) { cur_date = msg; return; } else { return cur_date; } }
+    if (query == 'schedule') { if (msg) { schedule = true; return; } else { return schedule; } }
     if (query == 'report-interview-time') {
         if (VERBOSE) { console.log('Next available appointment is at: ' + msg); }
-        else { console.log(msg); }
-        return;  
+        console.log(msg);
+        return;
     }
     if (query == 'fatal-error') {
         console.log('Fatal error: ' + msg);
@@ -74,103 +58,71 @@ page.onCallback = function(query, msg) {
 page.onLoadStarted = function() { loadInProgress = true; };
 page.onLoadFinished = function() { loadInProgress = false; };
 
-if (VERBOSE) { console.log('Please wait...'); }
-
-page.open(settings.init_url);
 var steps = [
-    function() { // Log in
+    function() {
         page.evaluate(function() {
             console.log('On GOES login page...');
             document.querySelector('input[name=username]').value = window.callPhantom('username');
             document.querySelector('input[name=password]').value = window.callPhantom('password');
-            document.querySelector('form[action="/pkmslogin.form"]').submit();
+            document.querySelector('input[name="Sign In"]').click();
+        });
+    },
+    function() {
+        page.evaluate(function() {
             console.log('Logging in...');
+            document.querySelector('a[href="/main/goes/HomePagePreAction.do"]').click();
         });
     },
-    function() { // Accept terms
+    function() {
         page.evaluate(function() {
-            
-            function fireClick(el) {
-                var ev = document.createEvent("MouseEvents");
-                ev.initEvent("click", true, true);
-                el.dispatchEvent(ev);
-            }
-            
-            var $acceptTermsBtn = document.querySelector('a[href="/main/goes/HomePagePreAction.do"]');
-
-            if (!$acceptTermsBtn) {
-                return window.callPhantom('fatal-error', 'Unable to find terms acceptance button');
-            }
-
-            fireClick($acceptTermsBtn);
             console.log('Accepting terms...');
+            document.querySelector('.bluebutton[name=manageAptm]').click();
         });
     },
-    function() { // main dashboard
+    function() {
         page.evaluate(function() {
-
-            function fireClick(el) {
-                var ev = document.createEvent("MouseEvents");
-                ev.initEvent("click", true, true);
-                el.dispatchEvent(ev);
-            }
-            
-            var $manageAptBtn = document.querySelector('.bluebutton[name=manageAptm]');
-            if (!$manageAptBtn) {
-                return window.callPhantom('fatal-error', 'Unable to find Manage Appointment button');
-            }
-
-            fireClick($manageAptBtn);
             console.log('Entering appointment management...');
+            // Current date XXX: clean up this search
+            date = document.querySelector(".maincontainer p:nth-child(7)").innerHTML.replace(/<strong>[\s\S]*?<\/strong>/, "");
+	    window.callPhantom('cur_date', date);
+            document.querySelector('input[name=reschedule]').click();
+
         });
     },
     function() {
         page.evaluate(function() {
-
-            function fireClick(el) {
-                var ev = document.createEvent("MouseEvents");
-                ev.initEvent("click", true, true);
-                el.dispatchEvent(ev);
-            }
-            
-            var $rescheduleBtn = document.querySelector('input[name=reschedule]');
-    
-            if (!$rescheduleBtn) {
-                return window.callPhantom('fatal-error', 'Unable to find reschedule button. Is it after or less than 24 hrs before your appointment?');
-            }
-
-            fireClick($rescheduleBtn);
             console.log('Entering rescheduling selection page...');
+            document.querySelector('select[name=selectedEnrollmentCenter]').value = window.callPhantom('enrollment_location_id');
+            document.querySelector('input[name=next]').click();
         });
     },
     function() {
         page.evaluate(function() {
-
-            function fireClick(el) {
-                var ev = document.createEvent("MouseEvents");
-                ev.initEvent("click", true, true);
-                el.dispatchEvent(ev);
-            }
-
-            document.querySelector('select[name=selectedEnrollmentCenter]').value = settings.enrollment_location_id.toString();
-            fireClick(document.querySelector('input[name=next]'));
             console.log('Choosing SFO...');
-        });
-    },
-    function() {
-
-        page.evaluate(function() {
 
             // We made it! Now we have to scrape the page for the earliest available date
-            
+
             var date = document.querySelector('.date table tr:first-child td:first-child').innerHTML;
             var month_year = document.querySelector('.date table tr:last-child td:last-child div').innerHTML;
-
-            var full_date = month_year.replace(',', ' ' + date + ',');
-            // console.log('');
-            window.callPhantom('report-interview-time', full_date)
-            // console.log('The next available appointment is on ' + full_date + '.');
+            var fut_date = month_year.replace(',', ' ' + date + ',');
+	    var cur_date = window.callPhantom('cur_date');
+	    console.log(fut_date + ' ? ' + cur_date);
+	    if( Date.parse(fut_date).valueOf() < Date.parse(cur_date).valueOf() ) {
+	        window.callPhantom('schedule', true);
+		console.log('Sooner appt available: ' + fut_date);
+		console.log(document.querySelector('.entry'));
+	    	document.querySelector('a[href="#"].entry').onmouseup()
+	    }
         });
+    },
+    function() {
+	if( window.callPhantom('schedule') ) {
+	    page.evaluate( function() {
+                document.querySelector('input[name=comments]').value = "Earlier appointment";
+                document.querySelector('input[name=Confirm]').click();
+            } );
+	}
+	return;
     }
 ];
 
@@ -180,7 +132,8 @@ interval = setInterval(function() {
     if (typeof steps[i] != "function") {
         return phantom.exit();
     }
-
+    var cur_date;
+    var schedule = false;
     steps[i]();
     i++;
 
